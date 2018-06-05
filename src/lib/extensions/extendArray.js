fb621eb8295318c608bae744a90371c437eeaafb
Andrew Salib <andezzat@gmail.com>
2018-06-05 15:06:03 +1000

    Attempt at abstracting some work onto Object and Array prototype funcs

--- src/lib/extensions/extendArray.js
+++ src/lib/extensions/extendArray.js
@@ -16,6 +16,15 @@ const arrayExtensions = {
     const values = this.map(x => x[prop]);
     return Math.max(...values);
   },
+  mostCommon(prop) {
+    const tally = {};
+    this
+      .map(x => x[prop])
+      .forEach(val => tally[val] ? tally[val]++ : tally[val] = 1);
+    
+    const max = Math.max(Object.values(tally));
+    return tally.find(val => val === max);
+  }
 };
 
 
--- src/lib/extensions/extendObject.js
+++ src/lib/extensions/extendObject.js
@@ -1,3 +1,14 @@
+const objectExtensions = {
+  find(predicate) {
+    const keys = Object.keys(this);
+    keys.forEach(k => {
+      if (predicate(this[k], k, this)) return this[k];
+    });
+  },
+};
+
 module.exports = () => {
-	Object.clone = obj => ({...obj });
+  Object.clone = obj => ({...obj });
+  
+  Object.keys(objectExtensions).forEach(ext => Object.prototype[ext] = objectExtensions[ext]);
 };
