d512416ef84c469de12b4888c58beca37f3ae2fb
Andrew Salib <andezzat@gmail.com>
2018-06-11 22:10:21 +1000

    fix drawing

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,3 +1,21 @@
+const scaleToCanvas = (x, length, padding, offset = false) => {
+	const lengthPixels = (x * length)
+	const paddingDilation = (length - padding) / length;
+	const dilatedPixels = lengthPixels * paddingDilation;
+	return offset
+		? lengthPixels + padding / 2
+		: lengthPixels;
+};
+
+const drawCircle = (ctx, { x = 0, y = 0 }, width = 1, fill = false, colour = 'black') => {
+	ctx.fillStyle = colour;
+	ctx.translate(x, y);
+	ctx.beginPath();
+	ctx.arc(0, 0, width, 0, Math.PI*2, true);
+	fill ? ctx.fill() : ctx.stroke();
+	ctx.closePath();
+};
+
 const createCanvas = id => (res) => {
 	const { nodes } = res;
 	const canvas = document.getElementById(id);
@@ -8,21 +26,18 @@ const createCanvas = id => (res) => {
 	ctx.clearRect(0, 0, width, height);
 
 	nodes.forEach((node) => {
-		ctx.save();
-		ctx.fillStyle = node.colour;
-
 		const padding = 40;
-		const offset = {
-			x: padding * width,
+		const coords = {
+			x: scaleToCanvas(node.normalized.rooms, width, padding, true),
+			y: height - scaleToCanvas(node.normalized.area, height, padding, true),
 		};
-		const x = node.normalized.rooms * width;
-		const y = height - (node.normalized.area * height);
-
-		ctx.translate(x, y);
-		ctx.beginPath();
-		ctx.arc(0, 0, 5, 0, Math.PI*2, true);
-		ctx.fill();
-		ctx.closePath();
+		
+		ctx.save();
+		drawCircle(ctx, coords, 5, true, node.isKnown ? node.colour : 'grey');
+		if (!node.isKnown) {
+			const circleOfInfluence = scaleToCanvas(node.neighbours[2].distance, width, padding);
+			drawCircle(ctx, coords, circleOfInfluence, false, node.colour);
+		}
 		ctx.restore();
 	});
 };
@@ -32,5 +47,5 @@ console.log('IN!');
 window.onload = () => {
 	console.log('got em!');
 	fetch('http://localhost:3000/nodes')
-		.then(res => res.json().then(createCanvas('app')));
+		.then(res => res.json().then(createCanvas('canvas')));
 };
